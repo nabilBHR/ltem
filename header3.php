184bf677b5f65911b01a16d841bc1662f6d5f0d6
Nabil Bouhar <nbouhar@sephora.fr>
2021-03-15 04:43:18 +0100

    interface admin

---
+++ header3.php
@@ -0,0 +1,186 @@
+<?php
+
+session_start();
+require "db.php";
+
+if ($_SESSION['mailu'] == NULL) {
+  echo "<script language='javascript' type='text/javascript'>";
+  echo 'window.location.href = "connexion.php"';
+  echo "</script>";
+}
+
+if ($_SESSION['mailu'] != "admin@projet-ltem.com") {
+  echo "<script language='javascript' type='text/javascript'>";
+  echo 'window.location.href = "listeKits.php"';
+  echo "</script>";
+}
+
+if (isset($_POST['btndec']) or isset($_POST['btndec2'])) {
+  session_destroy();
+  header('location: index.php');
+  exit;
+}
+
+?>
+
+<!-- Header -->
+<header class="header1">
+
+  <!-- Header desktop -->
+  <div class="container-menu-header">
+
+    <div class="topbar">
+      <table>
+        <tr>
+          <th class="col-lg-12 col-md-6">
+            <div class="col-lg-12 col-md-6">
+              <div class="media">
+                <div class="d-flex">
+                  <div class="icon-box col-md-3 col-sm-4"><i class="fa fa-envelope"></i></a></div>
+                </div>
+                <div class="media-body">
+                  Mettre un truc ici ...
+                </div>
+              </div>
+            </div>
+          </th>
+        </tr>
+      </table>
+    </div>
+
+    <div style="background-color:#FF7900  ;" class="wrap_header">
+      <!-- Logo -->
+      <a href="#" class="logo">
+        <img src="images/icons/logo.png" alt="IMG-LOGO">
+      </a>
+      <!-- Menu -->
+      <div class="wrap_menu">
+        <nav class="menu">
+          <ul class="main_menu">
+            <div align="center">
+              <button style="width: 180px; " type="button" onclick="location.href = 'messagerie.php'" class="btn btn-dark"> <i class="fa fa-envelope-o" aria-hidden="true"></i> Messagerie</button>
+              <button style="width: 180px; " type="button" onclick="location.href = 'listKitsAdmin.php'" class="btn btn-dark"><i class="fa fa-microchip" aria-hidden="true"></i> Liste des kits</button>
+            </div>
+          </ul>
+        </nav>
+      </div>
+
+      <!-- Header Icon -->
+      <div class="header-icons">
+        <span class="linedivide1"></span>
+        <?php echo "Administrateur" ?>
+        <div class="header-wrapicon2">
+          <img src="images/icons/icon-header-01.png" class="header-icon1 js-show-header-dropdown" alt="ICON">
+          <!-- Header cart noti -->
+          <div class="header-cart header-dropdown" style="background-color:#EBF5FB ; border-radius: 30px ; ">
+            <div class="alert alert-secondary" role="alert">
+              <?php echo '<strong> Utilisateur : </strong><br>' . "Administrateur" . '<hr>' . '<strong> E-mail : </strong><br>' . $_SESSION['mailu']; ?>
+            </div>
+            <div class="header-cart-buttons">
+              <div class="header-cart-wrapbtn">
+                <!-- Button -->
+                <a href="parametres2.php" class="flex-c-m size1 bg1 bo-rad-20 hov1 s-text1 trans-0-4">
+                  Paramètres
+                </a>
+              </div>
+              <div class="header-cart-wrapbtn">
+                <form method="post">
+                  <input type="submit" name="btndec" value="Déconnexion" class="flex-c-m size1 bg1 bo-rad-20 hov1 s-text1 trans-0-4">
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Header Mobile -->
+  <div class="wrap_header_mobile" style="background-color:#FF7900  ;">
+    <!-- Logo moblie -->
+    <a href="#" class="logo-mobile">
+      <img src="images/icons/logo.png" alt="IMG-LOGO">
+    </a>
+    <!-- Button show menu -->
+    <div class="btn-show-menu">
+      <!-- Header Icon mobile -->
+      <div class="header-icons-mobile">
+        <span class="linedivide2"></span>
+        <?php echo "Administrateur"; ?>
+        <div class="header-wrapicon2">
+          <img src="images/icons/icon-header-01.png" class="header-icon1 js-show-header-dropdown" alt="ICON">
+          <!-- Header cart noti -->
+          <div class="header-cart header-dropdown" style="background-color:#EBF5FB ; border-radius: 30px ; ">
+            <div class="alert alert-secondary" role="alert">
+              <?php echo '<strong> Utilisateur : </strong><br>' . "Administrateur" . '<hr>' . '<strong> E-mail : </strong><br>' . $_SESSION['mailu']; ?>
+            </div>
+            <div class="header-cart-buttons">
+              <div class="header-cart-wrapbtn">
+                <!-- Button -->
+                <a href="parametres2.php" class="flex-c-m size1 bg1 bo-rad-20 hov1 s-text1 trans-0-4">
+                  Paramètres
+                </a>
+              </div>
+              <div class="header-cart-wrapbtn">
+                <form method="post">
+                  <input type="submit" name="btndec" value="Déconnexion" class="flex-c-m size1 bg1 bo-rad-20 hov1 s-text1 trans-0-4">
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="btn-show-menu-mobile hamburger hamburger--squeeze">
+      <span class="hamburger-box">
+        <span class="hamburger-inner"></span>
+      </span>
+    </div>
+  </div>
+
+  <!-- Menu Mobile -->
+  <div class="wrap-side-menu">
+    <nav class="side-menu">
+      <div">
+        <br>
+        <div class="col-lg-11 col-md-6">
+          <div class="media">
+            <div class="d-flex">
+              <div class="icon-box col-md-3 col-sm-4"><i class="fa fa-map-marker"></i></div>
+            </div>
+            <div class="media-body">
+              <p></p>Magasin Wa3ou Electronics <br />Rue Hassiba Ben Bouali, Alger centre.</p>
+            </div>
+          </div>
+        </div>
+        <hr>
+        <div class="col-lg-10 col-md-6">
+          <div class="media">
+            <div class="d-flex">
+              <div class="icon-box col-md-3 col-sm-4"><i class="fa fa-phone"></i></div>
+            </div>
+            <div class="media-body">
+              <a href="tel:+1109171234567">0558947335</a> <br>
+              <a href="tel:+1101911897654">0555678854</a>
+            </div>
+          </div>
+        </div>
+        <hr>
+        <div class="col-lg-10 col-md-6">
+          <div class="media">
+            <div class="d-flex">
+              <div class="icon-box col-md-3 col-sm-4"><i class="fa fa-envelope"></i></a></div>
+            </div>
+            <div class="media-body">
+              Mettre un truc ici ...
+            </div>
+          </div>
+        </div>
+        <br>
+        <div align="center" style="background-color:#FF7900  ;">
+          <button style="width: 180px; margin:2px" type="button" onclick="location.href = 'messagerie.php'" class="btn btn-dark"> <i class="fa fa-envelope-o" aria-hidden="true"></i> Messagerie</button> </br>
+          <button style="width: 180px; " type="button" onclick="location.href = 'listKitsAdmin.php'" class="btn btn-dark"><i class="fa fa-microchip" aria-hidden="true"></i> Liste des kits</button></br>
+        </div>
+    </nav>
+  </div>
+</header>
--- parametres2.php
+++ parametres2.php
@@ -3,7 +3,7 @@
 
 <head>
 
-	<title>Connexion</title>
+	<title>Paramètres</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -41,100 +41,10 @@
 </head>
 
 <body class="animsition">
-	<?php
-	require "header1.php";
-
-	$erreur = '';
-
-	if (isset($_POST['btnCon'])) {
-		$email = $_POST['email'];
-		$stmt = mysqli_prepare($bdd, 'SELECT email, motPasse , confirmMail , blocage FROM utilisateur where email = ? LIMIT 1');
-		mysqli_stmt_bind_param($stmt, "s", $email);
-		mysqli_stmt_execute($stmt);
-		mysqli_stmt_store_result($stmt);
-		$verifmail = mysqli_stmt_num_rows($stmt);
-
-		if ($verifmail == 1) {
-			mysqli_stmt_bind_result($stmt, $donnees['email'], $donnees['motPasse'], $donnees['confirmMail'], $donnees['blocage']);
-			mysqli_stmt_fetch($stmt);
-			$bloc = $donnees['blocage'];
-			$valid = $donnees['confirmMail'];
-			$mdp = $donnees['motPasse'];
-
-			if ($bloc == 1) {
-				// utilisateur bloqué
-				$erreur = '<div class="alert alert-danger alert-dismissible" role="alert">
-				<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<strong> Votre compte est bloqué !</strong>
-				</div>';
-			} else if ($valid == 0) {
-				// compte non validé
-				$erreur = '<div class="alert alert-danger alert-dismissible" role="alert">
-				<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<strong>Adresse mail pas encore validée !</strong>
-				<hr>
-				<p>Veuillez cliquer sur le lien de validation envoyé par mail.<p>
-				</div>';
-			} else if ($mdp != $_POST['motPasse']) {
-				// mot de passe incorrect 
-				$erreur = '<div class="alert alert-danger alert-dismissible" role="alert">
-				<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<strong> Mot de passe incorrect ! </strong>
-				</div>';
-			} else {
-
-				// Utilisateur connecté !
-				$_SESSION['mailu'] = $email;
-				if ($_POST['email'] == "admin@projet-ltem.com") {
-					echo "<script language='javascript' type='text/javascript'>";
-					echo 'window.location.href = "listKitsAdmin.php"';
-					echo "</script>";
-				} else {
-					echo "<script language='javascript' type='text/javascript'>";
-					echo 'window.location.href = "listeKits.php"';
-					echo "</script>";
-				}
-			}
-		} else {
-			//mail incorrect 
-			$erreur = '<div class="alert alert-danger alert-dismissible" role="alert">
-			<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-			<strong> il n\'existe pas de compte pour cette adresse mail ! </strong>
-			</div>';
-		}
-	}
-	?>
-
+	<?php require "header3.php"; ?>
 	<div class="card bg-light ">
-		<div align="center" class="card-header">Connexion</div>
-		<div class="card-body">
-			<!-- formulaire connexion-->
-			<DIV ALIGN="CENTER">
-				<div class="container">
-					<div class="col-md-6 p-b-30">
-						<form class="leave-comment" method="post">
-							<?php echo $erreur; ?>
-							<div class="bo5 of-hidden size15  col-auto m-b-20">
-								<input class="sizefull s-text7 p-l-22 p-r-22" type="email" name="email" placeholder="Votre adresse e-mail" required='required'>
-							</div>
-							<div class="bo5 of-hidden size15  col-auto m-b-20">
-								<input class="sizefull s-text7 p-l-22 p-r-22" type="password" name="motPasse" placeholder="Votre mot de passe" required='required'>
-							</div>
-							<div class="w-size25">
-								<button type="submit" name="btnCon" class="flex-c-m size2 bg1 bo-rad-23 hov1 m-text3 trans-0-4">
-									Se connecter
-								</button>
-							</div>
-							<br>
-					</div>
-					<h5>Vous n'êtes pas encore membre ? <a href="inscription.php"> Inscrivez-vous !</a></h5>
-				</div>
-				</form>
-			</div>
-		</div>
+		<img class="fit-picture" src="images/page-under-construction.jpg" alt="Page under construction">
 	</div>
-
-	<?php require "footer.php" ?>
 
 	<!--===============================================================================================-->
 	<script type="text/javascript" src="libs/vendor/jquery/jquery-3.2.1.min.js"></script>
@@ -143,9 +53,6 @@
 	<!--===============================================================================================-->
 	<script type="text/javascript" src="libs/vendor/bootstrap/js/popper.js"></script>
 	<script type="text/javascript" src="libs/vendor/bootstrap/js/bootstrap.min.js"></script>
-	<!--===============================================================================================-->
-	<script type="text/javascript" src="libs/vendor/slick/slick.min.js"></script>
-	<script type="text/javascript" src="libs/js/slick-custom.js"></script>
 	<!--===============================================================================================-->
 	<script src="libs/js/main.js"></script>
 	<!--===============================================================================================-->
